f65aa829a452928697881a8b0bbe6a2cecdbd40c
Keks <keks@htmlacademy.ru>
2022-07-27 22:45:35 +0300

    :heavy_check_mark: Сборка #3

--- 3/gulpfile.js
+++ 3/gulpfile.js
@@ -4,9 +4,13 @@ const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass')(require('sass'));
 const inlineCss = require('gulp-inline-css');
 
+const IS_PRODUCTION = (process.env.NODE_ENV || 'production') === 'production';
+const TEMP_DIR = 'tmp';
+const DEST_DIR = IS_PRODUCTION ? './' : 'tmp/';
+
 function server() {
   browserSync.init({
-    server: { baseDir: 'build/' },
+    server: { baseDir: DEST_DIR },
     notify: false,
     online: true,
   })
@@ -14,43 +18,43 @@ function server() {
 
 function html() {
   return gulp.src(['src/**/*.html'])
-    .pipe(gulp.dest('build/'))
+    .pipe(gulp.dest(DEST_DIR))
     .pipe(inlineCss({
       applyStyleTags: true,
       applyLinkTags: true,
       preserveMediaQueries: true,
       applyWidthAttributes: false,
       applyTableAttributes: false,
-      removeHtmlSelectors: true,
+      removeHtmlSelectors: IS_PRODUCTION,
     }))
-    .pipe(gulp.dest('build/'))
+    .pipe(gulp.dest(DEST_DIR))
 }
 
 function styles() {
   return gulp.src(['src/**/styles/**/*.scss', '!src/sass/**'], { base: 'src' })
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('build/'))
+    .pipe(gulp.dest(DEST_DIR))
 }
 
 function images() {
   return gulp.src(['src/**/img/**/*.*'], { base: 'src' })
-    .pipe(gulp.dest('build/'));
+    .pipe(gulp.dest(DEST_DIR));
 }
 
 function watch() {
   gulp.watch(['src/**/*.(html|scss)'], gulp.series(styles, html));
   gulp.watch(['src/**/img/**/*'], images);
 
-  gulp.watch(['build/**/*.html']).on('change', browserSync.reload);
-  gulp.watch(['build/**/img/**/*']).on('change', browserSync.reload);
+  gulp.watch([`${DEST_DIR}**/*.html`]).on('change', browserSync.reload);
+  gulp.watch([`${DEST_DIR}**/img/**/*`]).on('change', browserSync.reload);
 }
 
 function clean() {
-  return del('build', { force: true });
+  return del(TEMP_DIR, { force: true });
 }
 
 function cleanup() {
-  return del('build/**/styles', { force: true });
+  return del(['./**/styles', '!src/**'], { force: true });
 }
 
 const build = gulp.series(clean, styles, html, images);
